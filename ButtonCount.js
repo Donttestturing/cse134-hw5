482e625ab5c9333652f4f4d83723b90b8bb1a108
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2023-03-04 19:32:32 -0800

    add style to shadow button

--- ButtonCount.js
+++ ButtonCount.js
@@ -1,6 +1,6 @@
 
   class ButtonCount extends HTMLElement {
-    
+
     constructor() {
       super();
 
@@ -9,6 +9,7 @@
 
         const buttonArea = document.createElement('button');
         buttonArea.innerText = `Times Clicked: ${count}`;
+        buttonArea.style = `background-color: var(--color-primary); border: 3px ridge var(--color-dark-trim); border-radius: 3px; font-weight: 700;`;
 
         buttonArea.addEventListener('click', countButton)
         function countButton () {
