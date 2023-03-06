3604754f3a65904ea17d63ce5a15c0b0fb6ed501
Brendan Devlin <52110466+Donttestturing@users.noreply.github.com>
2023-03-05 19:57:14 -0800

    add basic get,post,put stuff

--- methodtest.js
+++ methodtest.js
@@ -12,29 +12,26 @@
         buttons[0].addEventListener('click', () => {            
             outputArea.innerText = '';
 
-            const myForm = document.querySelector('form');
-            const formData = new FormData(myForm);
-        /*    for (const value of formData.values()) {
-                 console.log(value);
-            } 
-        */
+                const myForm = document.querySelector('form');
+                const formData = new FormData(myForm);
+            /*    for (const value of formData.values()) {
+                    console.log(value);
+                } 
+            */
 
-        fetch("https://httpbin.org/post", {method: 'POST', body: formData})
-        .then(response => response.json())
-        .then(result => {
-            console.log('POST Success:');
-   
-            setTimeout(()=>{
-                outputArea.innerText = JSON.stringify(result, null, 3);
-            }, 10);
+            fetch("https://httpbin.org/post", {method: 'POST', Headers:{'Content-Type': 'application/x-www-form-urlencoded'}, body: formData})
+            .then(response => response.json())
+            .then(result => {
+                console.log('POST Success:');
+    
+                setTimeout(()=>{
+                    outputArea.innerText = JSON.stringify(result, null, 5);
+                }, 10);
 
-        })
-        .catch(error => {
-            console.error('POST Error:', error);
-        });
-
-
-
+            })
+            .catch(error => {
+                console.error('POST Error:', error);
+            });
 
 
         });
@@ -42,6 +39,34 @@
 
 //GET        
         buttons[1].addEventListener('click', () => {
+            outputArea.innerText = '';
+
+            const myForm = document.querySelector('form');
+            const formData = new FormData(myForm);
+            //let data = [...formData.entries()];
+
+            const qString = new URLSearchParams(formData).toString();
+
+            let returnStr = `/?${qString}`
+            console.log(returnStr);
+        
+            
+            let getUrl = "https://httpbin.org/get" + returnStr;
+
+
+            fetch(getUrl, {method: 'GET', Headers:{'Accept': 'application/json', 'Accept-Encoding':'application/json'} })
+            .then(response => response.json()) //console.log(typeof response)
+            .then(result => {
+                console.log('GET Success:');
+    
+                setTimeout(()=>{
+                    outputArea.innerText = JSON.stringify(result, null, 5);
+                }, 10);
+
+            })
+            .catch(error => {
+                console.error('GET Error:', error);
+            });
 
             
         });
@@ -49,6 +74,25 @@
 
 //PUT        
         buttons[2].addEventListener('click', () => {
+            outputArea.innerText = '';
+
+            const myForm = document.querySelector('form');
+            const formData = new FormData(myForm);
+
+            fetch('https://httpbin.org/put', {method: 'PUT', Headers:{'Content-Type': 'application/x-www-form-urlencoded'}, body: formData })
+            .then(response => response.json()) 
+            .then(result => {
+                console.log('PUT Success:');
+    
+                setTimeout(()=>{
+                    outputArea.innerText = JSON.stringify(result, null, 5);
+                }, 10);
+
+            })
+            .catch(error => {
+                console.error('PUT Error:', error);
+            });
+
 
             
         });
@@ -56,6 +100,28 @@
 
 //DELETE    
         buttons[3].addEventListener('click', () => {
+            outputArea.innerText = '';
+
+            const myForm = document.querySelector('form');
+            const formData = new FormData(myForm);
+
+            fetch('https://httpbin.org/delete', {method: 'DELETE', Headers:{'Content-Type': 'application/x-www-form-urlencoded'}, body: formData })
+            .then(response => response.json()) 
+            .then(result => {
+                console.log('DELETE Success:');
+    
+                setTimeout(()=>{
+                    outputArea.innerText = JSON.stringify(result, null, 5);
+                }, 10);
+
+            })
+            .catch(error => {
+                console.error('DELETE Error:', error);
+            });
+
+
 
                     
         });
+
+        
